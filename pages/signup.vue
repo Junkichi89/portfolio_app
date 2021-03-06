de71d77df96341ae47516d2165179c969f67ec62
Jumpei Asanuma <jumpei@JumpeinoMacBook-Pro.local>
2021-03-06 14:21:50 +0900

    first commit

--- pages/signup.vue
+++ pages/signup.vue
@@ -23,12 +23,14 @@
                   <v-card-text class="pa-0">
                     <v-form lazy-validation>
                       <v-text-field
+                        v-model="newUser.email"
                         label="メールアドレス"
                         required
                         validate-on-blur
                       />
                       <v-text-field
                         ref="register_password"
+                        v-model="newUser.password"
                         label="パスワード"
                         required
                         validate-on-blur
@@ -38,7 +40,11 @@
                       <v-alert v-if="registerErrorMsg" dense text type="error">
                         {{ registerErrorMsg }}
                       </v-alert>
-                      <v-btn color="blue darken-3" class="mr-4 white--text">
+                      <v-btn
+                        color="blue darken-3"
+                        class="mr-4 white--text"
+                        @submit="signUp"
+                      >
                         登録
                       </v-btn>
                     </v-form>
@@ -62,11 +68,51 @@
   </v-container>
 </template>
 <script>
+import zxcvbn from 'zxcvbn'
 export default {
   data() {
     return {
       tab: null,
+      newUser: {
+        password: '',
+        email: '',
+      },
+      emailRules: [
+        (v) => {
+          if (v) {
+            return (
+              /.+@.+\..+/.test(v) || '有効なメールアドレスを入力してください'
+            )
+          } else {
+            return true
+          }
+        },
+      ],
+      register_passwordRules: [
+        (v) => !!v || 'パスワードを入力してください',
+        (v) =>
+          zxcvbn(v).score >= 3 ||
+          '大文字・小文字・数字・記号を混ぜた強いパスワードにしてください',
+      ],
+      register_passwordAgainRules: [
+        (v) => {
+          if (v) {
+            return (
+              this.$refs.register_password.value === v ||
+              'パスワードと一致しません'
+            )
+          } else {
+            return true
+          }
+        },
+      ],
+      show_registerPassword: false,
     }
+  },
+  methods: {
+    signUp(newUser) {
+      this.$store.dispatch('auth/signUp', newUser)
+    },
   },
 }
 </script>
---
+++ store/auth.js
@@ -0,0 +1,31 @@
+import firebase from '~/plugins/firebase'
+
+// const db = firebase.firestore()
+const auth = firebase.auth()
+// const usersRef = db.collection('users')
+
+// ログイン情報の保持
+// 新規登録
+/**
+ * signupからデータを受け取り、firebaseでユーザー登録
+ * 登録データを反映させる。
+ */
+
+export const state = () => ({
+  users: [],
+})
+
+export const actions = {
+  async signup(newUser) {
+    await auth
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then((user) => {
+        console.log('signed in')
+      })
+      .catch((error) => {
+        console.log(error)
+        // var errorCode = error.code
+        // var errorMessage = error.message
+      })
+  },
+}
