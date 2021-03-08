f066e0dbfe4ec11e431dea1e6e00ea70aa24416a
Jumpei Asanuma <jumpei@JumpeinoMacBook-Pro.local>
2021-03-08 10:19:46 +0900

    #5バリデーション等追加

--- pages/signup.vue
+++ pages/signup.vue
@@ -21,22 +21,52 @@
               <v-col sm="12">
                 <v-card flat>
                   <v-card-text class="pa-0">
-                    <v-form lazy-validation>
+                    <v-form lazy-validation v-model="register_valid">
                       <v-text-field
                         v-model="email"
                         label="メールアドレス"
+                        :rules="emailRules"
                         required
                         validate-on-blur
                       />
                       <v-text-field
                         ref="register_password"
-                        v-model="password"
+                        v-model="register_password"
                         label="パスワード"
+                        :append-icon="
+                          show_registerPassword ? 'mdi-eye' : 'mdi-eye-off'
+                        "
+                        :type="show_registerPassword ? 'text' : 'password'"
+                        :rules="register_passwordRules"
                         required
                         validate-on-blur
+                        @click:append="
+                          show_registerPassword = !show_registerPassword
+                        "
                       >
+                        <template #progress>
+                          <v-progress-linear
+                            :value="score.value"
+                            :color="score.color"
+                            absolute
+                            height="2"
+                          />
+                        </template>
                       </v-text-field>
-                      <v-text-field label="パスワード（確認）" required />
+                      <v-text-field
+                        v-model="register_password_again"
+                        label="パスワード（確認）"
+                        required
+                        :append-icon="
+                          show_registerPassword ? 'mdi-eye' : 'mdi-eye-off'
+                        "
+                        :type="show_registerPassword ? 'text' : 'password'"
+                        validate-on-blur
+                        :rules="register_passwordAgainRules"
+                        @click:append="
+                          show_registerPassword = !show_registerPassword
+                        "
+                      />
                       <v-alert v-if="registerErrorMsg" dense text type="error">
                         {{ registerErrorMsg }}
                       </v-alert>
@@ -72,8 +102,10 @@ import zxcvbn from 'zxcvbn'
 export default {
   data() {
     return {
+      register_valid: true,
       tab: null,
-      password: '',
+      register_password: '',
+      register_password_again: '',
       email: '',
       emailRules: [
         (v) => {
@@ -108,11 +140,46 @@ export default {
       registerErrorMsg: 'Sorry',
     }
   },
+  computed: {
+    progress() {
+      return this.score.value
+    },
+    score() {
+      const result = zxcvbn(this.register_password)
+      switch (result.score) {
+        case 4:
+          return {
+            color: 'green',
+            value: 100,
+          }
+        case 3:
+          return {
+            color: 'light-green lighten-1',
+            value: 75,
+          }
+        case 2:
+          return {
+            color: 'amber accent-2',
+            value: 50,
+          }
+        case 1:
+          return {
+            color: 'deep-orange lighten-1',
+            value: 25,
+          }
+        default:
+          return {
+            color: 'red darken-3',
+            value: 0,
+          }
+      }
+    },
+  },
   methods: {
     signUp() {
       const newUser = {
         email: this.email,
-        password: this.password,
+        password: this.register_password,
       }
 
       this.$store.dispatch('auth/signUp', newUser)
